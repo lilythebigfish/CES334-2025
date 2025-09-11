ac7606f29241efcc7dc38a68050164964ea8c00b
lilythebigfish <lilylin0201@hotmail.com>
2025-09-11 16:05:38 -0400

    updated config files and ip

--- raspberryPi/login_script.sh
+++ raspberryPi/login_script.sh
@@ -1,6 +1,6 @@
 echo "Starting script..."
 
-cp /boot/firmware/config.txt /student334/CES334-2025/raspberryPi/configs/
+cp /boot/firmware/config.txt ./configs
 hostname -I | awk '{print $1}' > ~/CES334-2025/raspberryPi/configs/ip.md
 git add .
 git commit -m"updated config files and ip"
