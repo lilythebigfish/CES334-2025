013ddc929e2f54c205e8360318496fe8fca0ef6f
lilythebigfish <lilylin0201@hotmail.com>
2025-09-18 14:48:54 -0400

    made labnotes folder

--- raspberryPi/login_script.sh
+++ raspberryPi/login_script.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 echo "Starting script..."
 
 cp /boot/firmware/config.txt ./configs
-echo "$(date '+%Y-%m-%d %H:%M:%S') - $(hostname -I | awk '{print $1}')" >> ~/CES334-2025/raspberryPi/configs/ip.md
+echo "$(date '+%Y-%m-%d %H:%M:%S') - $(hostname -I | awk '{print $1}')\n" >> ~/CES334-2025/raspberryPi/configs/ip.md
 git add .
 git commit -m"updated config files and ip"
 git push
